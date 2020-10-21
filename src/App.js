a038cbd2e5e80750826635a8f940762608669399
Olivia Tufte <ortufte@gmail.com>
2020-10-21 17:36:49 -0500

    create signup and login components, create post fetch for a new user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/signup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    user: null, 
+    signUpForm: {
+      name: "",
+      email: "",
+      password: "",
+    }
+
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      signUpForm: {
+        ...this.state.signUpForm,
+        [name]: value
+      }
+    })
+  }
+
+  handleSignUp = event => {
+    event.preventDefault();
+
+    let user = this.state.signUpForm
+
+    fetch("http://localhost:3000/api/v1/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(
+        { user }
+      )
+    })
+    .then(resp => resp.json())
+    .then(json => 
+      this.setState({ user: json }))
+    .catch(err => console.error("Error:", err));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        Modern Wellness Frontend 
+        <SignUp 
+        handleInputChange={this.handleInputChange}
+        handleSignUp={this.handleSignUp}
+        name={this.state.signUpForm.name}
+        email={this.state.signUpForm.email}
+        password={this.state.signUpForm.password}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/signup.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const SignUp = (props) => {
+
+    return (
+        <form onSubmit={props.handleSignUp}>
+            <input 
+            type="text" 
+            name="name" 
+            placeholder="Name"
+            onChange={props.handleInputChange}
+            value={props.name}
+            />
+            <input 
+            type="text" 
+            name="email" 
+            placeholder="Email Address"
+            onChange={props.handleInputChange}
+            value={props.email}
+            />
+            <input 
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={props.handleInputChange}
+            value={props.password}
+            />
+              <input 
+            type="text"
+            name="affliction"
+            placeholder="Illness"
+            onChange={props.handleInputChange}
+            value={props.affliction}
+            />
+            <input 
+            type="submit" 
+            name="Sign Up" 
+            />
+        </form>
+    )
+}
+
+export default SignUp
