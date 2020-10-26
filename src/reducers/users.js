5f04d97da914da9364f25f84037417c7f6828657
Olivia Tufte <ortufte@gmail.com>
2020-10-26 16:27:58 -0500

    create uusers reducer

---
+++ src/reducers/users.js
@@ -0,0 +1,11 @@
+export default ( state = [], action ) => { // the state needs to be set equal to an array or an object depending on which data type the reducer is responsible for returning.
+    switch (action.type) {
+        
+        
+        
+        
+        default:
+            return state
+    }
+
+}
